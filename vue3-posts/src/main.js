b18d000d7dd73d22f3ce48ef11fbf61f8cc2801d
JangJongWook <kowo1001@dongguk.edu>
2023-02-09 05:21:24 +0900

    first

---
+++ vue3-posts/src/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/js/bootstrap.js';
+
+createApp(App).mount('#app');
+// import './assets/main.css';
