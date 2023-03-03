b78181c719f17f5b937b2a419d51b184d03049ce
Jongwook Jang <jwjang@gnewsoft.com>
2023-03-03 12:37:57 +0900

    23-03-03

--- vue3-posts/src/main.js
+++ vue3-posts/src/main.js
@@ -7,3 +7,8 @@ import router from '@/router';
 createApp(App).use(router).mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
 // import './assets/main.css';
+
+console.log('MODE: ', import.meta.env.MODE);
+console.log('BASE_URL: ', import.meta.env.BASE_URL);
+console.log('PROD: ', import.meta.env.PROD);
+console.log('DEV: ', import.meta.env.DEV);
